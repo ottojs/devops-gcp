9eb2a2f284189574aa791049c7a4efb48a48ff0d
Ryan Lelek <ryanlelek@gmail.com>
2024-07-27 00:42:55 -0500

    Edit: Move files bucket to modules

--- files_bucket.tf
+++ files_bucket.tf
@@ -1,40 +1,9 @@
 
-###############################
-##### File Uploads Bucket #####
-###############################
-
-# https://console.cloud.google.com/storage/browser
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
-resource "google_storage_bucket" "files" {
-  project                     = var.project_id
-  name                        = var.files_domain
-  location                    = "US"
-  storage_class               = "STANDARD"
-  uniform_bucket_level_access = true
-  public_access_prevention    = "enforced" # "inherited"
-  force_destroy               = !var.deletion_protection
-  versioning {
-    enabled = true
-  }
-  logging {
-    log_bucket        = "gcp-logs-${var.project_id}"
-    log_object_prefix = "gcp-logs-${var.project_id}/${var.files_domain}"
-  }
-
-  cors {
-    origin          = var.lb_cors_origins
-    method          = var.lb_cors_methods
-    response_header = var.lb_cors_headers
-    max_age_seconds = 3600
-  }
-
-  lifecycle_rule {
-    action {
-      type = "Delete"
-    }
-    condition {
-      days_since_noncurrent_time = 3
-      no_age                     = true
-    }
-  }
+# Used for file uploads (in the future)
+module "files_bucket" {
+  source              = "./modules/bucket_private"
+  project_id          = var.project_id
+  name                = "files.${var.domain_root}"
+  deletion_protection = var.deletion_protection
+  log_bucket_name     = module.log_bucket.name
 }
