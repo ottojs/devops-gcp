7afd6451f96f8676cb4dcb46d99e1b76093298d8
Ryan Lelek <ryanlelek@gmail.com>
2024-10-27 16:54:12 -0500

    Dependencies: Upgrading provider google to v6.8.0

--- provider.tf
+++ provider.tf
@@ -11,7 +11,7 @@ terraform {
     google = {
       # https://registry.terraform.io/providers/hashicorp/google/latest/docs
       source  = "hashicorp/google"
-      version = "6.5.0"
+      version = "6.8.0"
     }
   }
 }
