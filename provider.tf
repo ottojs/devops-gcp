557605e54b754e2653df8ca2cef935c15a810767
Ryan Lelek <ryanlelek@gmail.com>
2025-04-27 14:50:38 -0500

    Dependencies: Upgrading providers

--- provider.tf
+++ provider.tf
@@ -6,12 +6,12 @@
 # tofu init -upgrade
 
 terraform {
-  required_version = ">= 1.8"
+  required_version = ">= 1.9"
   required_providers {
     google = {
       # https://registry.terraform.io/providers/hashicorp/google/latest/docs
       source  = "hashicorp/google"
-      version = "6.17.0"
+      version = "6.32.0"
     }
   }
 }
